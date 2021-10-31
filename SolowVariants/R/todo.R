a0ed66c84e1efa0f0c30b59019d2c1f5adb30252
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-10-31 16:21:29 +0100

    minor edits

---
+++ SolowVariants/R/todo.R
@@ -0,0 +1,11 @@
+call <- function(){
+my.name <- readline(prompt="Enter name: ")
+my.age <- readline(prompt="Enter age: ")
+# convert character into integer
+my.age <- as.integer(my.age)
+print(paste("Hi,", my.name, "next year you will be", my.age+1, "years old."))
+}
+call()
+
+
+# create such an interactive function to easily simulate teh solow models
