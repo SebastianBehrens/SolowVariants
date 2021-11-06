7b8625c264231928d08b18e80ed8f7c613cb9f7b
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-11-06 02:39:19 +0100

    working update

---
+++ R/workingdoc.R
@@ -0,0 +1,14 @@
+getRequiredParams("ESEGRomer")
+getRequiredStartingValues("ESEGRomer")
+aux_np <- 500
+aux_parameter_grid <- create_parameter_grid(
+  c("alpha", "phi", "lambda", "rho", "s", "sR", "delta", "n"),
+  c(1/3, 1/3, 0.2, 0.2, 0.2, 0.15, 0.15, 0.005),
+  c(NA, NA, NA, NA, NA, NA, NA, NA),
+  c(NA, NA, NA, NA, NA, NA, NA, NA),
+  aux_np
+)
+aux_startvalues <- list(A = 1, K = 1, L = 1)
+
+aux_simulation <- SimulateExtendedSolowModelEndogenousGrowthRomer(aux_parameter_grid, aux_np, aux_startvalues)
+VisualiseSimulation(aux_simulation, c("gY"), "free")
