cee15e66eb34cd7c2bbaeb3896262d2e46bfd771
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-11-06 02:42:48 +0100

    edit

---
+++ workingdoc_loadpackage.R
@@ -0,0 +1,5 @@
+# roxygen2::roxygenise()
+# devtools::install()  
+# devtools::install_github("SebastianBehrens/SolowVariants", build_vignettes = T, force = T)
+# library(SolowVariants)
+# vignette("ESEG")
