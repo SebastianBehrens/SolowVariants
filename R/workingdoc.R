58ba85016d61f89d123cceabe3d18c54cfa80839
Sebastian J. Billy Behrens <51058351+SebastianBehrens@users.noreply.github.com>
2021-11-08 15:40:19 +0100

    minor edit

--- R/workingdoc.R
+++ R/workingdoc.R
@@ -1,13 +1,13 @@
-getRequiredParams("ESEGRomer")
-getRequiredStartingValues("ESEGRomer")
-aux_parameter_grid <- create_parameter_grid(
-  c("alpha", "phi", "lambda", "rho", "s", "sR", "delta", "n"),
-  c(1/3, 1/3, 0.2, 0.2, 0.2, 0.15, 0.15, 0.005),
-  c(NA, NA, NA, NA, NA, NA, NA, NA),
-  c(NA, NA, NA, NA, NA, NA, NA, NA),
-  aux_np
-)
+# getRequiredParams("ESEGRomer")
+# getRequiredStartingValues("ESEGRomer")
+# aux_parameter_grid <- create_parameter_grid(
+#   c("alpha", "phi", "lambda", "rho", "s", "sR", "delta", "n"),
+#   c(1/3, 1/3, 0.2, 0.2, 0.2, 0.15, 0.15, 0.005),
+#   c(NA, NA, NA, NA, NA, NA, NA, NA),
+#   c(NA, NA, NA, NA, NA, NA, NA, NA),
+#   aux_np
+# )
 
-aux_startvalues
-aux_simulation <- SimulateExtendedSolowModelEndogenousGrowthRomer(aux_parameter_grid, aux_np, aux_startvalues)
-VisualiseSimulation(aux_simulation, c("gY", "L"), "fixed")
+# aux_startvalues
+# aux_simulation <- SimulateExtendedSolowModelEndogenousGrowthRomer(aux_parameter_grid, aux_np, aux_startvalues)
+# VisualiseSimulation(aux_simulation, c("gY", "L"), "fixed")
